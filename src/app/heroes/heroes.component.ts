43fa7c1e4bd1321fe0208614ff2750b94a91bcc5
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-11-24 23:31:02 +0900

    pt4 終了 ルーターを追加 @Input デコレータが不要になったので削除（リンクになった） シンプルなルート、リダイレクトするルート、パラメータ付きルートを定義できた 複数のコンポーネント間で HeroService を共有した

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;  // 変数定義 Hero 型
 
   constructor(
     private heroService: HeroService
@@ -18,11 +17,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  // メソッド作成　引数は hero 変数。型は Hero 型 app/hero.ts で定義
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;   // 定義した selectedHero 変数に、onSelect イベントで取得した値を代入
   }
 
   getHeroes(): void {
