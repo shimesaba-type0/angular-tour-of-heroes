9e52667302b2f8f4b8e62c87cc6c61ada4c973ec
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-11-24 17:27:48 +0900

    *ngFor, *ngIf, イベントバインディングをまなんだ

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Naruto'
-  };
+  // heores 変数に HEROES 配列を代入している
+  heroes = HEROES;
+  selectedHero: Hero;  // 変数定義 Hero 型
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // メソッド作成　引数は hero 変数。型は Hero 型 app/hero.ts で定義
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;   // 定義した selectedHero 変数に、onSelect イベントで取得した値を代入
+  }
 }
