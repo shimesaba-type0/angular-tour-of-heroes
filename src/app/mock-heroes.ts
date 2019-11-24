8103d4473c0abdeb68b27139b40adcd99d93f6c3
shimesaba-type0 <t.k.shimesaba@gamil.com>
2019-11-24 21:32:18 +0900

    HeorDetailComponent をつくった。親が子を制御できるように、プロパティバインディングを学んだ。@Input デコレータを使った。

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Hokage' },
+    { id: 12, name: 'Naruto' },
+    { id: 13, name: 'Saske' },
+    { id: 14, name: 'Bolto' },
+    { id: 15, name: 'Rock Lee' },
+    { id: 16, name: 'Kakashi' },
+    { id: 17, name: 'Itachi' },
+    { id: 18, name: 'Orochimaru' },
+    { id: 19, name: 'Gaara' },
+    { id: 20, name: 'Jiraiya' }
+];
